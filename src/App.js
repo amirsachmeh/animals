8f4b3d4a7a44cef1edc8d1797898fc3cd7908449
amirsachmeh <amirhs.dma@gmail.com>
2023-07-27 00:51:24 +0330

    finsl

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import { useState } from "react";
 import AnimalShow from "./AnimalShow";
 
@@ -29,9 +30,9 @@ function App(){
    return <AnimalShow type={animal} key={index}/>
   })
   return(
-    <div>
+    <div className='app'>
     <button onClick={handleClick}>Add Animal</button>
-    <div>{renderedAnimals }</div>
+    <div className='animal-list'>{renderedAnimals }</div>
   </div>
     );
 }
